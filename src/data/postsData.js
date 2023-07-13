a9aaf11139c5a2a7cbe3aa856cd6ec52854d67df
Олександр <moskaletzsanych@gmail.com>
2023-07-13 21:45:31 +0300

    posts-added

---
+++ src/data/postsData.js
@@ -0,0 +1,94 @@
+const postsData = [
+  {
+    id: 1,
+    title: 'Beautiful Sunset',
+    image: 'https://via.placeholder.com/500x300',
+    location: 'Beach',
+    comments: [
+      {
+        id: 1,
+        text: 'Amazing view! I wish I could be there.',
+        createdAt: new Date('2022-01-01T09:30:00'),
+      },
+      {
+        id: 2,
+        text: 'The colors are so vibrant. Best sunset ever!',
+        createdAt: new Date('2022-01-02T18:45:00'),
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Exploring the Mountains',
+    image: 'https://via.placeholder.com/500x300',
+    location: 'Mountains',
+    comments: [
+      {
+        id: 1,
+        text: 'Incredible view! Nature at its best.',
+        createdAt: new Date('2022-01-03T11:20:00'),
+      },
+      {
+        id: 2,
+        text: 'I love hiking in the mountains. So peaceful.',
+        createdAt: new Date('2022-01-04T15:10:00'),
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'City Adventures',
+    image: 'https://via.placeholder.com/500x300',
+    location: 'City',
+    comments: [
+      {
+        id: 1,
+        text: 'The city lights are mesmerizing at night.',
+        createdAt: new Date('2022-01-05T20:55:00'),
+      },
+      {
+        id: 2,
+        text: 'So many things to explore in the city!',
+        createdAt: new Date('2022-01-06T14:30:00'),
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Serene Forest',
+    image: 'https://via.placeholder.com/500x300',
+    location: 'Forest',
+    comments: [
+      {
+        id: 1,
+        text: 'Walking through the forest is so calming.',
+        createdAt: new Date('2022-01-07T16:45:00'),
+      },
+      {
+        id: 2,
+        text: 'The fresh air in the forest is invigorating.',
+        createdAt: new Date('2022-01-08T10:20:00'),
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Sunrise on the Lake',
+    image: 'https://via.placeholder.com/500x300',
+    location: 'Lake',
+    comments: [
+      {
+        id: 1,
+        text: 'The sunrise over the lake is breathtaking.',
+        createdAt: new Date('2022-01-09T07:15:00'),
+      },
+      {
+        id: 2,
+        text: 'Early morning reflections on the lake are so peaceful.',
+        createdAt: new Date('2022-01-10T09:10:00'),
+      },
+    ],
+  },
+];
+
+export default postsData;
--- src/screens/PostsScreen.js
+++ src/screens/PostsScreen.js
@@ -1,7 +1,42 @@
+import { Feather } from '@expo/vector-icons';
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
+import postsData from '../data/postsData';
 
 const PostsScreen = () => {
+  const renderItem = ({ item }) => (
+    <View style={styles.postContainer}>
+      <Image
+        style={styles.postImage}
+        source={{ uri: item.image }}
+        resizeMode="cover"
+      />
+      <View style={styles.postContent}>
+        <Text style={styles.postTitle}>{item.title}</Text>
+        <View style={styles.postInfo}>
+          <View style={styles.postInfoWrapper}>
+            <Feather
+              name="message-circle"
+              size={24}
+              color="#BDBDBD"
+              style={styles.postInfoIcon}
+            />
+            <Text style={styles.postComments}>{item.comments.length}</Text>
+          </View>
+          <View style={styles.postInfoWrapper}>
+            <Feather
+              name="map-pin"
+              size={24}
+              color="#BDBDBD"
+              style={styles.postInfoIcon}
+            />
+            <Text style={styles.postLocation}>{item.location}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <View style={styles.userContainer}>
@@ -15,6 +50,13 @@ const PostsScreen = () => {
           <Text style={styles.userEmail}>johndoe@example.com</Text>
         </View>
       </View>
+      <FlatList
+        data={postsData}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        contentContainerStyle={styles.postsContainer}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
@@ -47,6 +89,45 @@ const styles = StyleSheet.create({
   userEmail: {
     fontSize: 11,
     color: '#212121',
+  },
+  postsContainer: {
+    padding: 0,
+  },
+  postContainer: {
+    flexDirection: 'column',
+    marginBottom: 32,
+  },
+  postComments: {
+    color: '#BDBDBD',
+    fontSize: 16,
+    fontWeight: 'regular',
+  },
+  postInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  postInfoIcon: {},
+  postInfoWrapper: { flexDirection: 'row', gap: 6 },
+  postImage: {
+    width: '100%',
+    height: 240,
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  postContent: {
+    // marginLeft: 8,
+  },
+  postTitle: {
+    fontSize: 16,
+    fontWeight: 'medium',
+    color: '#212121',
+    marginBottom: 8,
+  },
+  postLocation: {
+    fontSize: 16,
+    color: '#212121',
+    fontWeight: 'regular',
+    textDecorationLine: 'underline',
   },
 });
 
