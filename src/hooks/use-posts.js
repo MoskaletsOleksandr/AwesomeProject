a3338f0f2cc2aa6751e3e6834deb82a9a6584efd
Олександр <moskaletzsanych@gmail.com>
2023-08-05 07:30:36 +0300

    sorting

--- src/hooks/use-posts.js
+++ src/hooks/use-posts.js
@@ -3,7 +3,9 @@ import { useSelector } from 'react-redux';
 export function usePosts() {
   const { allPosts } = useSelector((state) => state.posts);
 
+  const sortedPosts = [...allPosts].sort((a, b) => b.createdAt - a.createdAt);
+
   return {
-    allPosts,
+    allPosts: sortedPosts,
   };
 }
